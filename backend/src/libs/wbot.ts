a8226512f5bae9d7185e3f1fea054c213a46cef7
Diego Armond <diego.armond@live.com>
2025-07-09 16:41:36 -0300

    wbot

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -11,7 +11,7 @@ import makeWASocket, {
   jidNormalizedUser,
   CacheStore
 } from "@whiskeysockets/baileys";
-import { makeInMemoryStore } from "@whiskeysockets/baileys";
+import makeInMemoryStore, { useMultiFileAuthState } from "@whiskeysockets/baileys";
 import { Op } from "sequelize";
 import { FindOptions } from "sequelize/types";
 import Whatsapp from "../models/Whatsapp";
